ec580aa0747d71de9d682897f813f6f006850bcd
JingL <ljwinnie@mlrmem05.hlt.utdallas.edu>
2017-06-06 23:00:07 -0500

    update GSR and dictionaries of validation set

--- UniversalPetrarch/utilities.py
+++ UniversalPetrarch/utilities.py
@@ -435,8 +435,8 @@ def combine_code(selfcode, to_add):
     combined value
 
     """
-
-    if to_add < 0:
+    
+    if to_add <= 0:
         return to_add + selfcode
     if selfcode >= 0x1000 and to_add >= 0x1000:
         return to_add  # If both verbs are high-level, take the lower nested one. I think this is what we want?
